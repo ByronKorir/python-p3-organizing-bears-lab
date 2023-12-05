0bb5e815c2ad30697238f74a957d560dc9f831a2
byron korir <byron.korir@student.moringaschool.com>
2023-12-05 13:00:28 +0300

    complete assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) VALUES 
+    ('Mr. Chocolate', 5, 'M', 'Brown', 'Calm', TRUE),
+    ('Rowdy', 8, 'F', 'Black', 'Aggressive', TRUE),
+    ('Tabitha', 3, 'M', 'Blond', 'Playful', TRUE),
+    ('Sergent Brown', 6, 'F', 'Brown', 'Shy', TRUE),
+    ('Melissa', 10, 'M', 'Cinnamon', 'Curious', TRUE),
+    ('Grinch', 4, 'F', 'Black', 'Energetic', TRUE),
+    ('Wendy', 7, 'M', 'Brown', 'Relaxed', TRUE),
+    (NULL, 2, 'F', 'Blond', 'Friendly', TRUE);
